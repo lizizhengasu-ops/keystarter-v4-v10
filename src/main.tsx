70a341a3ec4a609e055bef7dda5b233139861aca
lizizheng <lizizhengasu@gmail.com>
2026-07-28 12:11:43 +0800

    v6.1.24: full backup before AccountPage build fix - includes MainApp.tsx workaround

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import "./i18n"
-import App from "./App.tsx"
+import App from "./MainApp.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <App />
