529d170e507ebc1c431f85d76536492a82e65952
lizizheng <lizizhengasu@gmail.com>
2026-07-28 15:10:37 +0800

    v6.2.5: final - Plan C complete, 5 rounds done

---
+++ test_orders.php
@@ -0,0 +1,12 @@
+﻿<?php
+require "/var/www/keys-starter.com/wp-load.php";
+$orders = wc_get_orders(["limit" => 10]);
+echo "Orders found: " . count($orders) . "\n";
+foreach ($orders as $o) {
+    echo "#" . $o->get_id() . " | " . $o->get_status() . " | " . $o->get_billing_email() . " | key_email: " . ($o->get_meta("_key_email_status") ?: "-") . "\n";
+}
+echo "--- Users ---\n";
+$users = get_users(["number" => 5]);
+foreach ($users as $u) {
+    echo $u->user_email . " | " . implode(",", $u->roles) . "\n";
+}
